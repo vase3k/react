76c384d154414731d2f47e8038f5166d87ea2a90
Vasiliy Vinogradov <vase3k@gmail.com>
2023-11-04 22:39:45 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"overrides": [
+		{
+			"env": {
+				"node": true
+			},
+			"files": [
+				".eslintrc.{js,cjs}"
+			],
+			"parserOptions": {
+				"sourceType": "script"
+			}
+		}
+	],
+	"parserOptions": {
+		"ecmaVersion": "latest"
+	},
+	"rules": {
+		"linebreak-style": [
+			"error",
+			"windows"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"no-unused-vars": [
+			"warn"
+		]
+	}
+};
--- array.js
+++
@@ -1,1 +0,0 @@
-const array = [1, 2, 3]
