9de5d9b3f4cccd71faa6c392b18bdab1fdedc4c5
Vasiliy Vinogradov <vase3k@gmail.com>
2023-11-05 10:59:13 +0300

    application creation

--- JavaScript.02/array.js
+++ JavaScript.02/array.js
@@ -1,3 +1,26 @@
-const array = [1, 2, 3];
-let govno = 1;
-let govno = 1;
+/* const numberOfFilms = parseInt(prompt("Сколько фильмом вы уже смотрели?", ""));
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("moves", ""),
+    b = prompt("rating", ""),
+    c = prompt("moves", ""),
+    d = prompt("rating", "");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+ */
+const personalMovieDB2 = {
+    movies: {},
+};
+const a = "van dam",
+    b = 8.2;
+personalMovieDB2.movies2 = {};
+personalMovieDB2.movies2[a] = b;
+console.log(personalMovieDB2);
