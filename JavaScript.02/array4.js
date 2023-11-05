5b33277e5441faf9fc614d28e20884ba2a84a0ed
Vasiliy Vinogradov <vase3k@gmail.com>
2023-11-05 22:08:38 +0300

    cycle proctice

--- JavaScript.02/array4.js
+++ JavaScript.02/array4.js
@@ -33,3 +33,45 @@ first: for (let i = 0; i < 3; i++) {
         }
     }
 }
+
+for (let i = 5; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 20; i >= 10; i--) {
+    if (i == 13) break;
+    console.log(i);
+}
+
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+
+let i = 2;
+while (i <= 16) {
+    if (i % 2 === 0) {
+        i++;
+    } else {
+        console.log(i);
+        i++;
+    }
+}
+
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    } else continue;
+}
+
+(() => {
+    const arrayOfNumbers = [];
+    for (let i = 0; i <= 5; i++) {
+        arrayOfNumbers[i] = i + 5;
+    }
+    console.log(arrayOfNumbers);
+    return arrayOfNumbers;
+})();
