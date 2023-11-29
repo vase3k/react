d96158ce4d72d1550c676dc29c739fd7a7b429e6
Vasiliy Vinogradov <vase3k@gmail.com>
2023-11-29 13:15:27 +0300

    DOM

--- JavaScript.02/array4 copy 13.js
+++ JavaScript.02/array4 copy 13.js
@@ -1,60 +1,16 @@
 "use strict";
-
-const shoppingMallData = {
-    shops: [
-        {
-            width: 10,
-            length: 5
-        },
-        {
-            width: 15,
-            length: 7
-        },
-        {
-            width: 20,
-            length: 5
-        },
-        {
-            width: 8,
-            length: 10
-        }
-    ],
-    height: 5,
-    moneyPer1m3: 30,
-    budget: 50000
-};
-function isBudgetEnough(data) {
-    let acc = 0;
-    data.shops.forEach((element) => {
-        const m3 = element.width * element.length * data.height;
-        console.log(element.width, element.length, data.height);
-        acc += m3;
-    });
-    if (data.budget >= acc * data.moneyPer1m3) {
-        return "Бюджета достаточно";
-    } else return "Бюджета недостаточно";
+function hi() {
+    console.log("say hi!");
 }
-isBudgetEnough(shoppingMallData);
 
-const students = ["Peter", "Andrew", "Ann", "Mark", "Josh",
-    "Sandra", "Cris", "Bernard", "Takesi", "Sam"];
+hi();
 
-function sortStudentsByGroups(arr) {
-    arr.sort();
-    let result = [];
-    for (let i = 0; i < 3; i++) {
-        result.push(arr.splice(0, 3));
-    }
-    result.push("Оставшиеся студенты:");
-    if (arr.length === 0) {
-        result[3] += " -";
-    } else {
-        for (let j = 0; j < arr.length; j++) {
-            if (j === 0) {
-                result[3] += ` ${arr[j]}`;
-            } else result[3] += `, ${arr[j]}`;
-        }
-    }
-    return result;
+const arr = [1, 14, 4, 30, 54],
+    sorted = arr.sort(compareNum);
+
+function compareNum(a, b) {
+    a = 4, b = 14;
+    return a - b;
 }
-sortStudentsByGroups(students);
+
+console.log(sorted);
