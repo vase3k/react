dbecf91314db34e8689c8af7026610999b719bba
Vasiliy Vinogradov <vase3k@gmail.com>
2024-01-23 16:58:48 +0300

    123

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,49 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>one strock structure</h2>
+}
+
+/* const Field = () => {
+  const holder = 'Enter here';
+  const styleField = {
+    width: '300px'
+  }
+  return <input
+    placeholder={holder}
+    type="text"
+    style={styleField} />
+} */
+
+class Field extends Component {
+  render() {
+    const holder = 'Enter here';
+    const styleField = {
+      width: '300px'
+    };
+    return <input
+      placeholder={holder}
+      type="text"
+      style={styleField} />
+  }
+}
+
+function Btn() {
+  const text = "log in";
+  const logged = false;
+  return <button>{logged ? 'enter' : "wait"}</button >
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header />
+      <Field />
+      <Btn />
     </div>
   );
 }
 
+export { Header };
 export default App;
