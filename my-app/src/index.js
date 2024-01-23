9e684629f26966468bb253a1a5b9fbbf97ea5578
Vasiliy Vinogradov <vase3k@gmail.com>
2024-01-23 12:16:52 +0300

    122

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//const elem = <h2>hello world!</h2>;
+
+/* const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem,
+); */
+
+const text = 'hello world';
+
+const elem = (
+  <div>
+    <h2>{['asdas', 12, 231]}</h2>
+    <button tabIndex={0}>Click</button>
+    <button tabIndex="0">Click</button>
+  </div>
 );
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(
+    elem
+  )
