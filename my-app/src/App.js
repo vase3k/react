4027c09be4d3e812c6a9df37b32420bf3d3d6a4b
Vasiliy Vinogradov <vase3k@gmail.com>
2024-01-23 17:24:26 +0300

    124

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, StrictMode } from 'react';
 import './App.css';
 
 const Header = () => {
@@ -38,7 +38,9 @@ function Btn() {
 function App() {
   return (
     <div className="App">
-      <Header />
+      <StrictMode>
+        <Header />
+      </StrictMode>
       <Field />
       <Btn />
     </div>
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Header } from './App';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <App />
+    <StrictMode>
+      <App />
+    </StrictMode>
   );
