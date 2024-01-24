d96291f5aa38d2ed4dcd1f8a4085edb72710b4c4
Vasiliy Vinogradov <vase3k@gmail.com>
2024-01-24 14:39:01 +0300

    127

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -3,21 +3,32 @@ import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employee-add-form/employee-add-form';
-
-
 import './app.css';
+
+function WhoAmI({ name, surname, link }) {
+    return (
+        <div>
+            <h1>My name is {name()}, surname - {surname}</h1>
+            <a href={link}>My profile</a>
+        </div>
+    )
+}
 
 function App() {
     return (
-        <div className="app">
-            <AppInfo />
-
-            <div className="Search-panel">
-                <SearchPanel />
-                <AppFilter />
-            </div>
-            <EmployeesList />
-            <EmployeesAddForm />
+        /*         <div className="app">
+                    <AppInfo />
+        
+                    <div className="Search-panel">
+                        <SearchPanel />
+                        <AppFilter />
+                    </div>
+                    <EmployeesList />
+                    <EmployeesAddForm />
+                </div> */
+        <div className='app'>
+            <WhoAmI name={() => 'John'} surname="vinogradov" link="https://3dnews.ru/" />
+            <WhoAmI name={() => 'Alex'} surname="Gredov" link="https://3dnews.ru/" />
         </div>
     );
 }
