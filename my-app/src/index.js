ac4cca55f09d9df93af0ef3a5986b4a9a8320bcc
Vasiliy Vinogradov <vase3k@gmail.com>
2024-01-24 11:52:57 +0300

    126

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,12 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/app/app';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <StrictMode>
+    <React.StrictMode>
       <App />
-    </StrictMode>
+    </React.StrictMode>
   );
