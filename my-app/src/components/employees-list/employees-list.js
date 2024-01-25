c489719a562b88b2f9bcb3330da770908b52871b
Vasiliy Vinogradov <vase3k@gmail.com>
2024-01-25 12:47:29 +0300

    129

--- my-app/src/components/employees-list/employees-list.js
+++ my-app/src/components/employees-list/employees-list.js
@@ -5,8 +5,9 @@ import "./employees-list.css";
 const EmployeesList = ({ data }) => {
 
     const elements = data.map(item => {
+        const { id, ...itemProps } = item;
         return (
-            <EmployersListItem {...item} />
+            <EmployersListItem key={id} {...itemProps} />
         )
     })
 
