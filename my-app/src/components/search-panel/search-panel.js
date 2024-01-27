02642feedd9986cc4161ae8a79503db0a08e5239
Vasiliy Vinogradov <vase3k@gmail.com>
2024-01-27 20:29:58 +0300

    138

--- my-app/src/components/search-panel/search-panel.js
+++ my-app/src/components/search-panel/search-panel.js
@@ -1,12 +1,30 @@
+import { Component } from "react";
 import "./search-panel.css";
 
-const SearchPanel = () => {
-    return (
-        <input type="text"
-            className="form-control search-input"
-            placeholder="Найти сотрудника"
-        />
-    )
+class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ""
+        }
+    }
+
+    OnUpdateSearch = (e) => {
+        const term = e.target.value;
+        this.setState({ term });
+        this.props.OnUpdateSearch(term)
+    }
+
+    render() {
+        return (
+            <input type="text"
+                className="form-control search-input"
+                placeholder="Найти сотрудника"
+                value={this.state.term}
+                onChange={this.OnUpdateSearch}
+            />
+        )
+    }
 }
 
 export default SearchPanel;
