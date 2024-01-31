017358172c1e98677ffce66955357cc5c5eb59e0
Vasiliy Vinogradov <vase3k@gmail.com>
2024-01-31 11:22:25 +0300

    149

--- marvel/src/index.js
+++ marvel/src/index.js
@@ -4,9 +4,6 @@ import App from './components/app/App';
 import MarvelService from './services/MarvelServices';
 import './style/style.scss';
 
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters(1011052).then(res => res.data.results.forEach(item => console.log(item.name)));
 
 ReactDOM.render(
   <React.StrictMode>
