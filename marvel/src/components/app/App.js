f16403d3e87efc1c8b18f1eb23d6a55f2607d29d
Vasiliy Vinogradov <vase3k@gmail.com>
2024-02-01 13:47:45 +0300

    152

--- marvel/src/components/app/App.js
+++ marvel/src/components/app/App.js
@@ -7,6 +7,15 @@ import CharInfo from "../charInfo/CharInfo";
 import decoration from '../../resources/img/vision.png';
 
 class App extends Component {
+    state = {
+        selectedChar: null
+    }
+
+    onCharSelected = (id) => {
+        this.setState({
+            selectedChar: id
+        })
+    }
 
     render() {
         return (
@@ -15,8 +24,8 @@ class App extends Component {
                 <main>
                     <RandomChar />
                     <div className="char__content">
-                        <CharList />
-                        <CharInfo />
+                        <CharList onCharSelected={this.onCharSelected} />
+                        <CharInfo charId={this.state.selectedChar} />
                     </div>
                     <img className="bg-decoration" src={decoration} alt="vision" />
                 </main>
--- marvel/src/components/charList/CharList.js
+++ marvel/src/components/charList/CharList.js
@@ -39,16 +39,18 @@ class CharList extends Component {
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = { 'objectFit': 'unset' }
             }
+
             return (
                 <li className='char__item'
-                    key={item.id}>
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
                     <img src={item.thumbnail} alt={item.name} style={imgStyle} />
                     <div className='char__name'>{item.name}</div>
-                </li>
+                </li >
             )
         });
         return (
-            <ul className='char__grid'>
+            <ul className='char__grid' >
                 {items}
             </ul>
         )
