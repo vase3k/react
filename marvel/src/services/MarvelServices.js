25f002350becb01981c46ea33f6e7de7daf840a4
Vasiliy Vinogradov <vase3k@gmail.com>
2024-02-02 11:30:02 +0300

    154

--- marvel/src/services/MarvelServices.js
+++ marvel/src/services/MarvelServices.js
@@ -1,6 +1,8 @@
 class MarvelService {
     _apiBase = "https://gateway.marvel.com:443/v1/public/";
     _apiKey = "apikey=6ee9a5303f753669364710c0f16d2273";
+    _baseOffset = 210;
+
 
     getRessource = async (url) => {
         let res = await fetch(url);
@@ -12,8 +14,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getRessource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getRessource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
