7b380e5d04fc660bb4cec382755955d09d3d4bce
Vasiliy Vinogradov <vase3k@gmail.com>
2024-02-02 17:43:41 +0300

    155

--- marvel/src/components/charInfo/CharInfo.js
+++ marvel/src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MarvelService from '../../services/MarvelServices';
 import Spinner from '../spinner/Spinner';
@@ -125,8 +126,11 @@ const View = ({ char }) => {
             </ul>
         </>
     )
-
-
 }
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
+}
+
 
 export default CharInfo;
