d6f603f6b6583150efb5ec2f2c26bd0065dfee6e
Vasiliy Vinogradov <vase3k@gmail.com>
2024-02-06 10:24:13 +0300

    166

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,4 +1,4 @@
-import { Component, useState, useCallback, useEffect } from 'react';
+import { Component, useState, useCallback, useEffect, useMemo } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
@@ -55,6 +55,11 @@ import './App.css';
 //     }
 // }
 
+const countTotal = (num) => {
+    console.log('counting...');
+    return num + 10;
+}
+
 const Slider = (props) => {
 
     const [slide, setSlide] = useState(0);
@@ -79,22 +84,30 @@ const Slider = (props) => {
         setAutoplay(autoplay => !autoplay);
     }
 
+    const total = useMemo(() => {
+        return countTotal(slide);
+    }, [slide]);
+
+    const style = useMemo(
+        () => (
+            { color: slide > 4 ? 'red' : 'black' }
+        )
+        , [slide]
+    )
+
+    useEffect(
+        () => {
+            console.log('styles!');
+        }, [style]);
+
     return (
         <Container>
             <div className="slider w-50 m-auto">
-                {/* {
-                    getSomeImages().map(
-                        (url, i) => {
-                            return (
-                                <img key={i} className="d-block w-100" src={url} alt="slide" />
-                            )
-                        }
-                    )
-                } */}
 
                 <Slide getSomeImages={getSomeImages} />
 
                 <div className="text-center mt-5">Active slide {slide} <br />{autoplay ? 'auto' : null}</div>
+                <div style={style} className="text-center mt-5">Total slides: {total}</div>
                 {<div className="buttons mt-3">
                     <button
                         className="btn btn-primary me-2"
