2ce8790e86e94e0f2d6ab34638592b198d0c1ff4
Vasiliy Vinogradov <vase3k@gmail.com>
2024-02-08 09:25:55 +0300

    171

--- marvel/src/components/app/App.js
+++ marvel/src/components/app/App.js
@@ -20,7 +20,7 @@ const App = () => {
         <div className="app">
             <AppHeader />
             <main>
-                {/* <ErrorBoundary>
+                <ErrorBoundary>
                     <RandomChar />
                 </ErrorBoundary>
                 <div className="char__content">
@@ -31,8 +31,8 @@ const App = () => {
                         <CharInfo charId={selectedChar} />
                     </ErrorBoundary>
                 </div>
-                <img className="bg-decoration" src={decoration} alt="vision" /> */}
-                <ComicsList />
+                <img className="bg-decoration" src={decoration} alt="vision" />
+                {/* <ComicsList /> */}
             </main>
         </div>
     )
