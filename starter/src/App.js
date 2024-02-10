9e66c402bcdd52b57509d5db5d7743e2f994b30e
Vasiliy Vinogradov <vase3k@gmail.com>
2024-02-10 10:33:54 +0300

    178

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,77 +1,34 @@
-import { useState, memo, useCallback } from 'react';
-import { Container } from 'react-bootstrap';
+import { useState } from 'react';
 import './App.css';
 
-// class Form extends Component {
+import Form from './form';
+import dataContext from './context';
 
-//     shouldComponentUpdate(nextProps) {
-//         if (this.props.mail.name === nextProps.mail.name) {
-//             return false;
-//         } return true;
-//     }
-
-//     render() {
-//         console.log('render');
-//         return (
-//             <Container>
-//                 <form className="w-50 border mt-5 p-3 m-auto">
-//                     <div className="mb-3">
-//                         <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-//                         <input value={this.props.mail.name} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com" />
-//                     </div>
-//                     <div className="mb-3">
-//                         <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-//                         <textarea value={this.props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-//                     </div>
-//                 </form>
-//             </Container>
-//         )
-//     }
-// }
-
-// function propsCompare(prevProps, nextProps) {
-//     return prevProps.mail.name === nextProps.mail.name && prevProps.text === nextProps.text;
-// }
-
-const Form = memo((props) => {
-    console.log('render');
-    return (
-        <Container>
-            <form className="w-50 border mt-5 p-3 m-auto">
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-                    <input value={props.mail} type="email" className='form-control' id="exampleFormControlInput1" placeholder="name@example.com" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea value={props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                </div>
-            </form>
-        </Container>
-    )
-});
+const { Provider } = dataContext;
 
 function App() {
     const [data, setData] = useState({
-        mail: "name@example.com",
-        text: 'some text'
+        mail: "second@example.com",
+        text: 'another text',
+        forceChangeMail: forceChangeMail
     });
 
-    const onLog = useCallback(() => {
-        console.log('wow');
-    }, [])
+    function forceChangeMail() {
+        setData({ ...data, mail: 'test@gmail.com' })
+    }
 
     return (
-        <>
-            <Form mail={data.mail} text={data.text} onLog={onLog} />
+        <Provider value={{ something: 'something' }} >
+            <Form text={data.text} />
             <button
                 onClick={() => setData({
                     mail: "name@example.com",
-                    text: 'some text'
+                    text: 'some text',
+                    forceChangeMail: forceChangeMail
                 })}>
                 Click me
             </button>
-        </>
+        </Provider>
     );
 }
 
---
+++ starter/src/input.js
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+import dataContext from './context';
+
+const InputComponent = () => {
+
+    const context = useContext(dataContext);
+
+    return (
+        < input
+            value={context.mail}
+            type="email"
+            className='form-control'
+            placeholder="name@example.com"
+            onFocus={context.forceChangeMail} />
+    )
+}
+
+export default InputComponent;
