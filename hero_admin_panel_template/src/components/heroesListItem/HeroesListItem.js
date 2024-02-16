55d76174a9f8986855113d852ffd94d801b7f890
Vasiliy Vinogradov <vase3k@gmail.com>
2024-02-16 20:25:15 +0300

    199

--- hero_admin_panel_template/src/components/heroesListItem/HeroesListItem.js
+++ hero_admin_panel_template/src/components/heroesListItem/HeroesListItem.js
@@ -1,5 +1,5 @@
 
-const HeroesListItem = ({name, description, element, onDelete}) => {
+const HeroesListItem = ({ name, description, element, onDelete }) => {
 
     let elementClassName;
 
@@ -21,18 +21,18 @@ const HeroesListItem = ({name, description, element, onDelete}) => {
     }
 
     return (
-        <li 
+        <li
             className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}>
-            <img src="http://www.stpaulsteinbach.org/wp-content/uploads/2014/09/unknown-hero.jpg" 
-                 className="img-fluid w-25 d-inline" 
-                 alt="unknown hero" 
-                 style={{'objectFit': 'cover'}}/>
+            <img src="https://rare-gallery.com/mocahbig/391936-darth-revan-star-wars-lightsaber-knights-of-the-old-republic.jpg"
+                className="img-fluid w-25 d-inline"
+                alt="unknown hero"
+                style={{ 'objectFit': 'cover' }} />
             <div className="card-body">
-                
+
                 <h3 className="card-title">{name}</h3>
                 <p className="card-text">{description}</p>
             </div>
-            <span onClick={onDelete} 
+            <span onClick={onDelete}
                 className="position-absolute top-0 start-100 translate-middle badge border rounded-pill bg-light">
                 <button type="button" className="btn-close btn-close" aria-label="Close"></button>
             </span>
