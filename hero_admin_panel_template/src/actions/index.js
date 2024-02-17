62f9a7f033774c90b80d3b7615ab099a4b33825e
Vasiliy Vinogradov <vase3k@gmail.com>
2024-02-17 12:50:07 +0300

    206

--- hero_admin_panel_template/src/actions/index.js
+++ hero_admin_panel_template/src/actions/index.js
@@ -1,4 +1,4 @@
-import { createAction } from '@reduxjs/toolkit';
+import { bindActionCreators, createAction } from '@reduxjs/toolkit';
 
 export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching());
@@ -10,11 +10,13 @@ export const fetchHeroes = (request) => (dispatch) => {
 export const heroesFetching = createAction('HEROES_FETCHING');
 export const heroesFetched = createAction('HEROES_FETCHED');
 
-export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
+// export const heroesFetchingError = () => {
+//     return {
+//         type: 'HEROES_FETCHING_ERROR'
+//     }
+// }
+
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
 
 export const filtersFetching = () => {
     return {
@@ -42,16 +44,20 @@ export const activeFilterChanged = (filter) => {
     }
 }
 
-export const heroCreated = (hero) => {
-    return {
-        type: 'HERO_CREATED',
-        payload: hero
-    }
-}
+// export const heroCreated = (hero) => {
+//     return {
+//         type: 'HERO_CREATED',
+//         payload: hero
+//     }
+// }
 
-export const heroDeleted = (id) => {
-    return {
-        type: 'HERO_DELETED',
-        payload: id
-    }
-}
+export const heroCreated = createAction('HERO_CREATED');
+
+// export const heroDeleted = (id) => {
+//     return {
+//         type: 'HERO_DELETED',
+//         payload: id
+//     }
+// }
+
+export const heroDeleted = createAction('HERO_DELETED');
