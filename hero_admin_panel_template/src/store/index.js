fd516ebbee0d702fb3ef14c27fee0246462dfe6c
Vasiliy Vinogradov <vase3k@gmail.com>
2024-02-17 10:01:00 +0300

    202

--- hero_admin_panel_template/src/store/index.js
+++ hero_admin_panel_template/src/store/index.js
@@ -1,6 +1,15 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+};
 
 const enhancer = (createStore) => (...args) => {
     const store = createStore(...args);
@@ -19,10 +28,8 @@ const enhancer = (createStore) => (...args) => {
 
 const store = createStore(
     combineReducers({ heroes, filters }),
-    enhancer(
-        enhancer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    compose(applyMiddleware(stringMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 export default store;
