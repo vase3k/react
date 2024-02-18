170720284a244f1003a0d69c07da904fc0c7003d
Vasiliy Vinogradov <vase3k@gmail.com>
2024-02-18 15:24:52 +0300

    212

--- hero_admin_panel_template/src/components/heroesAddForm/HeroesAddForm.js
+++ hero_admin_panel_template/src/components/heroesAddForm/HeroesAddForm.js
@@ -1,18 +1,20 @@
-import { useHttp } from '../../hooks/http.hook';
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
+import store from '../../store';
 
-import { heroCreated } from '../heroesList/heroesSlice';
+import { selectAll } from '../heroesFilters/filtersSlice';
+import { useCreateHeroMutation } from '../../api/apiSlice';
 
 const HeroesAddForm = () => {
     const [heroName, setHeroName] = useState('');
     const [heroDescr, setHeroDescr] = useState('');
     const [heroElement, setHeroElement] = useState('');
 
-    const { filters, filtersLoadingStatus } = useSelector(state => state.filters);
-    const dispatch = useDispatch();
-    const { request } = useHttp();
+    const [createHero] = useCreateHeroMutation();
+
+    const { filtersLoadingStatus } = useSelector(state => state.filters);
+    const filters = selectAll(store.getState());
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
@@ -24,11 +26,7 @@ const HeroesAddForm = () => {
             element: heroElement
         }
 
-        request("http://localhost:3001/heroes", 'POST', JSON.stringify(newHero))
-            .then(res => console.log(res, 'Отправка успешна'))
-            .then(dispatch(heroCreated(newHero)))
-            .catch(err => console.log(err));
-
+        createHero(newHero).unwrap();
 
         setHeroName('');
         setHeroDescr('');
